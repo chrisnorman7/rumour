aabe0554a76c01d0b56c26661e25df04b8fd8cff
Chris Norman <chris.norman1989@outlook.com>
2025-01-12 18:17:32 +0000

    Added the `RoomListTile` widget.

---
+++ lib/src/widgets/room_list_tile.dart
@@ -0,0 +1,68 @@
+import 'package:backstreets_widgets/extensions.dart';
+import 'package:backstreets_widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_audio_games/flutter_audio_games.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../database/database.dart';
+import '../providers.dart';
+import '../screens/select_room_screen.dart';
+import 'play_sound_reference_semantics.dart';
+
+/// A list tile which shows a room.
+class RoomListTile extends ConsumerWidget {
+  /// Create an instance.
+  const RoomListTile({
+    required this.roomId,
+    required this.onChanged,
+    this.title = 'Room',
+    this.autofocus = false,
+    super.key,
+  });
+
+  /// The ID of the room to use.
+  final int roomId;
+
+  /// The function to call when the room changes.
+  final void Function(Room room) onChanged;
+
+  /// The title of the [ListTile].
+  final String title;
+
+  /// Whether the [ListTile] should be autofocused.
+  final bool autofocus;
+
+  /// Build the widget.
+  @override
+  Widget build(final BuildContext context, final WidgetRef ref) {
+    final value = ref.watch(roomProvider(roomId));
+    return value.when(
+      data: (final room) => PlaySoundReferenceSemantics(
+        soundReferenceId: room.ambianceId,
+        child: Builder(
+          builder: (final builderContext) => ListTile(
+            autofocus: autofocus,
+            title: Text(title),
+            subtitle: Text(room.name),
+            onTap: () => builderContext
+              ..stopPlaySoundSemantics()
+              ..pushWidgetBuilder(
+                (final _) => SelectRoomScreen(
+                  onChanged: onChanged,
+                  roomId: roomId,
+                  zoneId: room.zoneId,
+                ),
+              ),
+          ),
+        ),
+      ),
+      error: ErrorListTile.withPositional,
+      loading: () => ListTile(
+        autofocus: autofocus,
+        title: Text(title),
+        subtitle: const LoadingWidget(),
+        onTap: () {},
+      ),
+    );
+  }
+}
