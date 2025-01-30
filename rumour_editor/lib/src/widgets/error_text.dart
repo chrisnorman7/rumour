8959434804da934d237e7dec035b1438de216355
Chris Norman <chris.norman1989@outlook.com>
2025-01-30 12:06:27 +0000

    Make `stackTrace` nullable.

--- rumour_editor/lib/src/widgets/error_text.dart
+++ rumour_editor/lib/src/widgets/error_text.dart
@@ -3,11 +3,7 @@ import 'package:flutter/material.dart';
 /// A widget to show an [error] and a [stackTrace].
 class ErrorText extends StatelessWidget {
   /// Create an instance.
-  const ErrorText({
-    required this.error,
-    required this.stackTrace,
-    super.key,
-  });
+  const ErrorText({required this.error, required this.stackTrace, super.key});
 
   /// Create an instance from positional arguments.
   const ErrorText.withPositional(this.error, this.stackTrace, {super.key});
@@ -16,16 +12,16 @@ class ErrorText extends StatelessWidget {
   final Object error;
 
   /// The stack trace to show.
-  final StackTrace stackTrace;
+  final StackTrace? stackTrace;
 
   /// Build the widget.
   @override
   Widget build(final BuildContext context) => Text(
-        '$error\n$stackTrace',
-        style: const TextStyle(
-          fontSize: 18.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.red,
-        ),
-      );
+    '$error\n$stackTrace',
+    style: const TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.red,
+    ),
+  );
 }
